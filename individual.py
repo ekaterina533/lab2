9be52cffda96e4c3317d2f3cefecc4e8ea3c51c8
ekaterina533 <ekaterinahrcnk@gmail.com>
2021-03-24 22:00:50 +0300

    update

---
+++ individual.py
@@ -0,0 +1,9 @@
+a=input("cathet a")
+b=input("cathet b")
+a=int(a)
+b=int(b)
+from math import sqrt
+c=sqrt((a**2+b**2))
+P=a+b+c
+print(P)
+
---
+++ numbers.py
@@ -0,0 +1,9 @@
+print("enter a number")
+a = float(input())
+b = float(input())
+d = float(input())
+k = float(input())
+c=a+b
+t=d+k
+p= (c/t)
+print("%.2f" % p)
