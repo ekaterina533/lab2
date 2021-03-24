4889435c1fc25a8b1c1cd42471ea395389a5f02e
ekaterina533 <ekaterinahrcnk@gmail.com>
2021-03-25 00:08:45 +0300

    apdate 2

--- individual2.py
+++ individual2.py
@@ -1,10 +1,10 @@
-y=float(input("enter the angle"))
+y=float(input("enter the angle 0<y<=2pi"))
 h=30
 h1=(y/h)
 h2=(y//h)
 m=((h1-h2)*60)
 a=6*m
-print ("угол для минутной стрелки", a)
+print ("угол для минутной стрелки","%.2f" % a)
 print ("полных часов", h2)
-print ("полных минут", m)
+print ("полных минут","%.2f" % m)
 
